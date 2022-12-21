dde7488fd50420d4a447d956d1a3fbe04b59cfcc
Soumwadeep Guha <soumwadeep@gmail.com>
2022-12-21 10:27:32 +0530

    Initial Commit

---
+++ SdgShare/SdgShare_Backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'y7h9z03w',
+    dataset: 'production'
+  }
+})
---
+++ SdgShare/SdgShare_Backend/schemas/index.js
@@ -0,0 +1,7 @@
+import user from "./user"
+import postedBy from "./postedBy"
+import comment from "./comment"
+import save from "./save"
+import pin from "./pin"
+
+export const schemaTypes = [user,postedBy,comment,save,pin]
