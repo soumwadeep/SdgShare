2bcc186c5420a6787a1afcb2d156dd2c98a03ff1
Soumwadeep Guha <soumwadeep@gmail.com>
2022-12-21 10:45:06 +0530

    Added Home And Login Struct

--- SdgShare/SdgShare_Frontend/src/App.js
+++ SdgShare/SdgShare_Frontend/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import {Routes,Route,useNavigate} from 'react-router-dom'
+import Login from '../components/Login'
+import Home from '../container/Home'
 
 const App = () => {
   return (
-    <>
-      <h1 className='text-3xl font-bold underline'>Hello Soumwadeep!</h1>
-    </>
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
   )
 }
 
