f1db6b0124b3679ad48ce1c3e7815cf731da9ee2
Soumwadeep Guha <soumwadeep@gmail.com>
2022-12-24 11:01:35 +0530

    Added Google Login

--- SdgShare/SdgShare_Frontend/src/components/Login.jsx
+++ SdgShare/SdgShare_Frontend/src/components/Login.jsx
@@ -1,37 +1,52 @@
-import React from 'react'
-import {useNavigate} from 'react-router-dom';
-import {FcGoogle} from 'react-icons/fc';
-import sharevideo from '../assets/backgroundvideo.mp4';
-import logo from '../assets/logocropped.png';
-import {GoogleOAuthProvider} from '@react-oauth/google';
-import {GoogleLogin,googleLogout} from '@react-oauth/google';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import sharevideo from "../assets/backgroundvideo.mp4";
+import logo from "../assets/logocropped.png";
+import {
+  GoogleOAuthProvider,
+  GoogleLogin,
+  googleLogout,
+} from "@react-oauth/google";
+import { client } from "../client";
+
 const Login = () => {
-  const responseGoogle = (response) => 
-  {
-    localStorage.setItem('user',JSON.stringify(response.profileObj));
-    const {name,googleId,imageUrl,email} = response.profileObj;
+  const navigate = useNavigate();
+
+  const responseGoogle = (response) => {
+    localStorage.setItem("user", JSON.stringify(response));
+
+    const { clientId, name, imageUrl } = response;
+
     const doc = {
-      _id:googleId,
-      _type:"user",
-      userName:name,
-      image:imageUrl,
-    }
-  }
+      _id: clientId,
+      _type: "user",
+      userName: name,
+      image: imageUrl,
+    };
+
+    client.createIfNotExists(doc).then(() => {
+      navigate("/", { replace: true });
+    });
+  };
+
   return (
     <>
-      <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
-      <div className="flex justify-start items-center flex-col h-screen">
-        <div className="relative w-full h-full">
-          <video 
-          src={sharevideo}
-          type="video/mp4"
-          autoPlay 
-          loop 
-          controls={false}
-          muted 
-          className="w-full h-full object-cover"
-          />
-          <div className="absolute 
+      <GoogleOAuthProvider
+        clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
+      >
+        <div className="flex justify-start items-center flex-col h-screen">
+          <div className="relative w-full h-full">
+            <video
+              src={sharevideo}
+              type="video/mp4"
+              autoPlay
+              loop
+              controls={false}
+              muted
+              className="w-full h-full object-cover"
+            />
+            <div
+              className="absolute 
           flex 
           flex-col 
           justify-center 
@@ -40,44 +55,25 @@ const Login = () => {
           right-0 
           left-0 
           bottom-0 
-          bg-blackOverlay">
-            <div className="p-5">
-              <img src={logo} alt="logo"
-              width="150px"
-              />
-              <div className="shadow-2xl">
-               <GoogleLogin
-                clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
-                render={(renderProps)=>(
-                  <button type="button"
-                  className="bg-mainColor 
-                  flex 
-                  justify-center 
-                  items-center 
-                  p-2 
-                  mt-5
-                  rounded-lg 
-                  cursor-pointer 
-                  outline-none"
-                  onClick={renderProps.onClick}
-                  disabled={renderProps.disabled}
-                  >
-                    <FcGoogle className="mr-4"/>
-                    Sign In with Google
-                    </button>
-                    )}
-                onSuccess={responseGoogle}
-                onFailure={responseGoogle}
-                cookiePolicy="single_host_origin"
-                />
+          bg-blackOverlay"
+            >
+              <div className="p-5">
+                <img src={logo} alt="logo" width="150px" />
+
+                <div className="shadow-2xl">
+                  <GoogleLogin
+                    onSuccess={responseGoogle}
+                    onError={responseGoogle}
+                  />
+                </div>
+
               </div>
             </div>
           </div>
         </div>
-      </div>
       </GoogleOAuthProvider>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
